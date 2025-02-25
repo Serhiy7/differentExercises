256c988fc6f1a134fdff7b5fdaf035275733d4e4
Serhiy7 <ssukalo79@gmail.com>
2025-02-25 11:56:00 +0100

    1

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function isEven(num) {
-    if (num % 2 == 0) {
-        return true;
-    } else {
-        return false;
-    }
+
+function findMax(a, b, c) {
+    return Math.max(a, b, c);
 }
 
-console.log(isEven(4)); // true
+console.log(findMax(3, 7, 5)); // 7
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function isEven(num) {
+//     if (num % 2 == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(isEven(4)); // true
  
